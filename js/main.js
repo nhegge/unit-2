e30d4f7d78f675484a962e7a535374fdf360d23c
nhegge <nhegge@wisc.edu>
2026-02-26 21:41:34 -0600

    activity 6 submission

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-/*Modified code from using geojson with leaflet tutorial/geog575 github
+/*Modified code from using geojson with leaflet tutorials/geog575 github
 Written by Nolan Hegge 2/19/26
 This code is meant to read in data from CO2 emissions from energy production from 1960-2023 across each State
 Data found is complied in states_cleaned_final.csv and states_cleaned_final.geojson
 Data sources: https://www.eia.gov/environment/emissions/state  and  https://developers.google.com/public-data/docs/canonical/states_csv
 */
 
-//declare global map variable so it can be seen by all functions
+//declare map variable globally so all functions have access
 var map;
+var minValue;
 
-//function to create the leaflet map
+//create the map
 function createMap(){
-
-    //create the map and center it on the united states
+    //create the map
     map = L.map('map', {
         center: [39.5, -98.35],
         zoom: 4
     });
 
-    //add the OSM base tilelayer
+    //add OSM base tilelayer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
@@ -26,29 +26,199 @@ function createMap(){
     getData(map);
 };
 
-//function that retrieves the data and places it on the map (retrieving co2 emissions by state)
+//Import GeoJSON data
 function getData(map){
+    //load the data
     fetch("data/states_cleaned_final.geojson")
-		.then(function(response){
-			return response.json();
-		})
-		.then(function(json){
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff4561",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(json){
+            //calculate minimum data value
+            minValue = calculateMinValue(json);
+            //create an attributes array
+            var attributes = processData(json);
+            //call function to create proportional symbols
+            createPropSymbols(json, attributes);
+            createSequenceControls(attributes);
+        })
+};
+
+//function to get the minimum value of our data
+function calculateMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each state
+    for(var state of data.features){
+        //loop through each year
+        for(var year = 1960; year <= 2023; year++){
+            //get co2 for current year
+            var value = state.properties["co2_" + String(year)];
+            //add value to array
+            allValues.push(value);
+        }
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Apperance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+
+//function to build an attributes array from the data
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with co2 values
+        if (attribute.indexOf("co2_") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    return attributes;
+};
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Step 4: Assign the current attribute based on the first index of the attributes array
+    var attribute = attributes[0];
+
+    //create marker options
+    var options = {
+        fillColor: "#ff4561",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>State:</b> " + feature.properties.fullName + " (" + feature.properties.state_abr + ")</p>";
+
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>CO2 Emissions in " + year + ":</b> " + feature.properties[attribute] + " million metric tons</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius)
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, attributes){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(map);
+};
+
+//Create new sequence controls
+function createSequenceControls(attributes){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+    
+    //set slider attributes
+    document.querySelector(".range-slider").max = attributes.length - 1;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+    
+    //add step buttons
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse">Reverse</button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward">Forward</button>');
+
+    //replace button content with images
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.png'>")
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>")
+
+    //click listener for buttons
+    document.querySelectorAll('.step').forEach(function(step){
+        step.addEventListener("click", function(){
+            var index = document.querySelector('.range-slider').value;
+
+            //increment or decrement depending on button clicked
+            if (step.id == 'forward'){
+                index++;
+                //if past the last attribute, wrap around to first attribute
+                index = index > attributes.length - 1 ? 0 : index;
+            } else if (step.id == 'reverse'){
+                index--;
+                //if past the first attribute, wrap around to last attribute
+                index = index < 0 ? attributes.length - 1 : index;
             };
 
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
+            //update slider
+            document.querySelector('.range-slider').value = index;
+
+            //pass new attribute to update symbols
+            updatePropSymbols(attributes[index]);
         })
+    })
+
+    //input listener for slider
+    document.querySelector('.range-slider').addEventListener('input', function(){
+        //get the new index value
+        var index = this.value;
+
+        //pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
+};
+
+//Resize proportional symbols according to new attribute values
+function updatePropSymbols(attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add state to popup content string
+            var popupContent = "<p><b>State:</b> " + props.fullName + " (" + props.state_abr + ")</p>";
+
+            //add formatted attribute to popup content string
+            var year = attribute.split("_")[1];
+            popupContent += "<p><b>CO2 Emissions in " + year + ":</b> " + props[attribute] + " million metric tons</p>";
+
+            //update popup content
+            popup = layer.getPopup();
+            popup.setContent(popupContent).update();
+        };
+    });
 };
 
 document.addEventListener('DOMContentLoaded',createMap)
